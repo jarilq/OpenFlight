89df74fc9382431f5c95d33b4fb60264658c3c0c
Holly Newton <holnewt@users.noreply.github.com>
2015-11-09 14:13:28 -0600

    Update tres_claw_switch.c
    The file was changed to use the SGProps format.  It no longer needs pointers to global defs.  The SGProp and local variables are created before the functions start, and they are initially set in the init_mission function.  At the end of the program after the local variables have been adjusted, these local variables are used to set the variable values in SGProps.

--- Software/mission/tres_claw_switch.c
+++ Software/mission/tres_claw_switch.c
@@ -2,42 +2,75 @@
 #include <stdlib.h>
 #include <math.h>
 
-#include "../globaldefs.h"
+//#include "../globaldefs.h"
+#include "../props.hxx"
 #include "mission_interface.h"
 
-extern void init_mission(
-		struct sensordata *sensorData_ptr,	///< pointer to sensorData structure
-		struct nav *navData_ptr,			///< pointer to navData structure
-		struct mission *missionData_ptr		///< pointer to missionData structure
-){};
+//Output Properties
+///********MISSION variables***********
+static SGPropertyNode *claw_mode_node = NULL;
+static SGPropertyNode *claw_select_node = NULL;
 
-extern void get_mission(
-		struct sensordata *sensorData_ptr,	///< pointer to sensorData structure
-		struct nav *navData_ptr,			///< pointer to navData structure
-		struct mission *missionData_ptr		///< pointer to missionData structure
-){};
+//Input Properties
+///********INCEPTOR variables***********
+static SGPropertyNode *mode_node = NULL;
+static SGPropertyNode *selected_node = NULL;
 
-extern void run_mission(struct sensordata *sensorData_ptr, struct nav *navData_ptr, struct mission *missionData_ptr) {
-	double pilot_mode = sensorData_ptr->inceptorData_ptr->mode;
-	double pilot_select = sensorData_ptr->inceptorData_ptr->select;
+//Output local variables
+static double claw_mode = 0.0;
+static double claw_select = 0.0;
+
+//Input local variables
+static double mode = 0.0;
+static double selected = 0.0;
+
+extern void init_mission(){
+	//Property node initialization
+	//Output variables
+	claw_mode_node = fgGetNode("/mission/claw_mode");
+	claw_select_node = fgGetNode("mission/claw_select");
+
+	//Input variables
+	mode_node = fgGetNode("inceptor/mode");
+	selected_node = fgGetNode("inceptor/select");
+
+	// Read values from property tree
+	// Input local variables
+	mode = mode_node->getDoubleValue();
+	selected = selected_node->getDoubleValue();
+
+	// Initialize local output variables
+	claw_mode = 0.0;
+	claw_select = 0.0;
+};
+extern void get_mission(){};
+
+extern void run_mission() {
+
+	double pilot_mode = mode;
+	double pilot_select = selected;
 
 	if((pilot_mode > 0.5)&&(pilot_mode < 1.5)){
-		missionData_ptr -> claw_mode = 1;
+		claw_mode = 1;
 	}
 	else if(pilot_mode > 1.5){
-		missionData_ptr -> claw_mode = 2;
+		claw_mode = 2;
 	}
 	else{
-		missionData_ptr -> claw_mode = 0;
+		claw_mode = 0;
 	}
 
 	if((pilot_select > 0.5)&&(pilot_select < 1.5)){
-		missionData_ptr -> claw_select = 1;
+		claw_select = 1;
 	}
 	else if(pilot_select > 1.5){
-		missionData_ptr -> claw_select = 2;
+		claw_select = 2;
 	}
 	else{
-		missionData_ptr -> claw_select = 0;
+		claw_select = 0;
 	}
+
+	claw_mode_node->setDoubleValue(claw_mode);
+	claw_select_node->setDoubleValue(claw_select);
+
 }
