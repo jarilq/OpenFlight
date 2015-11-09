959d5986e19ac13a74c80511f3e251598a30bcae
Holly Newton <holnewt@users.noreply.github.com>
2015-11-09 15:14:06 -0600

    Update systemid_interface.h
    Changed to get rid of a need for globaldefs.h

--- Software/system_id/systemid_interface.h
+++ Software/system_id/systemid_interface.h
@@ -20,18 +20,10 @@
 /*
  *	\ingroup systemid_fcns
 */
-extern void init_system_id(double time, 			///< [sec], time since in autopilot mode
-		struct sensordata *sensorData_ptr,	///< pointer to sensorData structure
-		struct nav *navData_ptr,			///< pointer to navData structure
-		struct control *controlData_ptr,		///< pointer to controlData structure
-		struct mission *missionData_ptr
+extern void init_system_id(double time 			///< [sec], time since in autopilot mode
 		);
 
-extern void get_system_id(double time, 			///< [sec], time since in autopilot mode
-		struct sensordata *sensorData_ptr,	///< pointer to sensorData structure
-		struct nav *navData_ptr,			///< pointer to navData structure
-		struct control *controlData_ptr,		///< pointer to controlData structure
-		struct mission *missionData_ptr
+extern void get_system_id(double time 			///< [sec], time since in autopilot mode
 		);
 
 // Auxiliary Functions
@@ -41,10 +33,10 @@ extern void get_system_id(double time, 			///< [sec], time since in autopilot mo
  *	\sa doublet121()
  *	\ingroup systemid_fcns
 */
-double doublet(double t0,		///< [sec], start time
-		double currentTime,		///< [sec], current time
+double doublet(double t0_sec,		///< [sec], start time
+		double currentTime_sec,		///< [sec], current time
 		double duration,		///< [sec], period of doublet pulse
-		double amplitude		///< [rad], magnitude of doublet pulse
+		double amplitude_rad		///< [rad], magnitude of doublet pulse
 		);
 
 /// Auxiliary function that returns a 1-2-1 doublet signal
@@ -52,12 +44,12 @@ double doublet(double t0,		///< [sec], start time
  *	\sa doublet()
  *	\ingroup systemid_fcns
 */
-double doublet121(double t0,	///< [sec], start time
-		double currentTime,		///< [sec], current time
-		double dur1,			///< [sec], duration of first pulse
-		double dur2,			///< [sec], duration of second pulse
-		double dur3,			///< [sec], duration of third pulse
-		double amplitude		///< [rad], magnitude of pulse
+double doublet121(double t0_sec,	///< [sec], start time
+		double currentTime_sec,		///< [sec], current time
+		double durPulse1_sec,		///< [sec], duration of first pulse
+		double durPulse2_sec,		///< [sec], duration of second pulse
+		double durPulse3_sec,		///< [sec], duration of third pulse
+		double amplitude_rad		///< [rad], magnitude of pulse
 		);
 
 /// Auxiliary function that adds a orthogonal multi-sine to one surface
@@ -65,9 +57,9 @@ double doublet121(double t0,	///< [sec], start time
  *	\sa two_multi_sine(), three_multi_sine()
  *	\ingroup systemid_fcns
 */
-void one_multi_sine (double t,	///< [sec], current time
+void one_multi_sine (double t_sec,	///< [sec], current time
 		double *dsurf,			///< pointer to control surface
-		double amp				///< [rad], amplitude of sine signal
+		double amp_rad			///< [rad], amplitude of sine signal
 		);
 
 /// Auxiliary function that adds an orthogonal multi-sine to two surfaces
@@ -78,8 +70,8 @@ void one_multi_sine (double t,	///< [sec], current time
 void two_multi_sine (double t,	///< [sec], current time
 		double *dsurf1,			///< pointer to control surface 1
 		double *dsurf2,			///< pointer to control surface 2
-		double amp1,			///< [rad], amplitude of sine signal 1
-		double amp2				///< [rad], amplitude of sine signal 2
+		double amp1_rad,			///< [rad], amplitude of sine signal 1
+		double amp2_rad				///< [rad], amplitude of sine signal 2
 		);
 
 /// Auxiliary function that adds an orthogonal multi-sine to three surfaces
@@ -87,10 +79,10 @@ void two_multi_sine (double t,	///< [sec], current time
  *	\sa two_multi_sine(), three_multi_sine()
  *	\ingroup systemid_fcns
 */
-void three_multi_sine (double t,	///< [sec], current time
-		double *de,					///< pointer to elevator control surface
-		double *da,					///< pointer to aileron control surface
-		double*dr					///< pointer to rudder control surface
+void three_multi_sine (double t_sec,	///< [sec], current time
+		double *elevatorSurf,					///< pointer to elevator control surface
+		double *aileronSurf,					///< pointer to aileron control surface
+		double *rudderSurf					///< pointer to rudder control surface
 		);
 
 #endif /* SYSTEMID_INTERFACE_H_ */
