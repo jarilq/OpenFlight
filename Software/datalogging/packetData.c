5ef95c5c0cb4202536036c7c977809d9533d1662
Holly Newton <holnewt@users.noreply.github.com>
2015-12-11 10:47:20 -0600

    Create packetData.c
    This example shows how to store sensor data in packets (look at HDF5 packets for more information).  This file does not currently work with the datalogging makefile, but it will compile with the HDF5 compiler h5cc.

---
+++ Software/datalogging/packetData.c
@@ -0,0 +1,80 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright by The HDF Group.                                               *
+ * Copyright by the Board of Trustees of the University of Illinois.         *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of HDF5.  The full HDF5 copyright notice, including     *
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the files COPYING and Copyright.html.  COPYING can be found at the root   *
+ * of the source code distribution tree; Copyright.html can be found at the  *
+ * root level of an installed copy of the electronic HDF5 document set and   *
+ * is linked from the top-level documents page.  It can also be found at     *
+ * http://hdfgroup.org/HDF5/doc/Copyright.html.  If you do not have          *
+ * access to either file, you may request a copy from help@hdfgroup.org.     *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include "hdf5_hl.h"
+#include <stdlib.h>
+
+/*-------------------------------------------------------------------------
+ * Packet Table Fixed-Length Example
+ *
+ * Example program that creates a packet table and performs
+ * writes and reads.
+ *
+ *-------------------------------------------------------------------------
+ */
+
+int main(void)
+{
+ hid_t          fid;        /* File identifier */
+ hid_t          ptable;     /* Packet table identifier */
+
+ herr_t         err;        /* Function return status */
+ hsize_t        count;      /* Number of records in the table */
+
+ int            x;          /* Loop variable */
+int p;
+
+    /* Buffers to hold data */
+ int writeBuffer[5];
+
+   /* Initialize buffers */
+ for(x=0; x<5; x++)
+ {
+     writeBuffer[x]=x;
+ }
+
+    /* Create a file using default properties */
+ fid=H5Fcreate("packetData.h5",H5F_ACC_TRUNC,H5P_DEFAULT,H5P_DEFAULT);
+
+    /* Create a fixed-length packet table within the file */
+    /* This table's "packets" will be simple integers and it will use compression
+     * level 5. */
+ ptable = H5PTcreate_fl(fid, "Packet Test Dataset", H5T_NATIVE_INT, (hsize_t)100, 5);
+ if(ptable == H5I_INVALID_HID)
+     goto out;
+
+    /* Writes packets to the packet table */
+for(p=0; p<5; p++)
+{
+ err = H5PTappend(ptable, (hsize_t)1, &(writeBuffer[p]) );//appends more packets to the end
+ if(err < 0)
+     goto out;
+}
+
+    /* Close the packet table */
+ err = H5PTclose(ptable);
+ if(err < 0)
+     goto out;
+
+    /* Close the file */
+ H5Fclose(fid);
+
+ return 0;
+
+ out: /* An error has occurred.  Clean up and exit. */
+    H5PTclose(ptable);
+    H5Fclose(fid);
+    return -1;
+}
