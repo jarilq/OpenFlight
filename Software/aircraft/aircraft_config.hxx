43598229861680d58eab9c4a1110f2942ac6ada1
pipat001@github.umn.edu <pipat001@umn.edu>
2016-02-25 15:59:07 -0600

    adding aircraft config directory

---
+++ Software/aircraft/aircraft_config.hxx
@@ -0,0 +1,13 @@
+/*
+ * aircraft_config.hxx
+ *
+ *  Created on: Feb 25, 2016
+ *      Author: patipan
+ */
+
+#ifndef AIRCRAFT_AIRCRAFT_CONFIG_HXX_
+#define AIRCRAFT_AIRCRAFT_CONFIG_HXX_
+
+void get_config(hid_t);
+
+#endif /* AIRCRAFT_AIRCRAFT_CONFIG_HXX_ */
