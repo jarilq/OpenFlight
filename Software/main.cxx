808ec67459bd146557fd3f57403bd252aeab236a
pipat001@github.umn.edu <pipat001@umn.edu>
2016-03-03 14:03:46 -0600

    updated with current progress of FSW functions

--- Software/main.cxx
+++ Software/main.cxx
@@ -2,39 +2,40 @@
 #include <stdlib.h>
 #include <iostream>
 
-#include "props.hxx"
-#include "nav/nav_interface.h"
-#include "mission/mission_interface.h"
-#include "system_id/systemid_interface.h"
+//#include "nav/nav_interface.h"
+//#include "mission/mission_interface.h"
+//#include "system_id/systemid_interface.h"
 #include "aircraft/ac_config.hxx"
 #include "control/ndi_interface.hxx"
 #include "datalogging/datalog_interface.hxx"
-
-using namespace std;
+#include "props.hxx"
 
 int main(void)
 {
 	int autop = 1;
+
 	props = new SGPropertyNode;
 	double time;
-	/*init_dataAq();
-	init_sensorProc();*/
-	init_mission();
-	init_nav();
+
+	//init_dataAq();
+	//init_sensorProc();
+	//init_mission();
+	//init_nav();
 	//init_telemetry();
 	init_datalog();
 	get_config();
 	init_control();
 	//init_controlAl();
-	init_system_id(time);
+	//init_system_id(time);
 	//init_actuators();
 
-	while (1)
+	for (int i=0; i < 3; i++)
+	//while (1)
 	{
-		/*get_dataAq();
-		get_sensorProc();*/
-		get_mission();
-		get_nav();
+		//get_dataAq();
+		//get_sensorProc();
+		//get_mission();
+		//get_nav();
 		//get_telemetry();
 		get_datalog();
 
@@ -42,12 +43,13 @@ int main(void)
 		{
 			get_control();
 			//get_controlAl();
-			get_system_id(time);
+			//get_system_id(time);
 			//get_actuators();
 		}
 	}
-	close_mission();
-	close_nav();
-	close_system_id();
+	//close_mission();
+	//close_nav();
+	//close_system_id();
 	close_datalog();
+	close_control();
 }
